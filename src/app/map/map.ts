c2e363e7ecbf35d5212676ebe2d5d447a70bfd94
random-aemn <82000272+random-aemn@users.noreply.github.com>
2025-06-30 20:59:25 -0400

    integrated leaflet via npm installs; added map component; failing on app.component.html

---
+++ src/app/map/map.ts
@@ -0,0 +1,33 @@
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  imports: [],
+  templateUrl: './map.html',
+  styleUrl: './map.css'
+})
+
+export class Map  implements AfterViewInit{
+
+
+  private map;
+
+  constructor() { }
+
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+  }
+
+    ngAfterViewInit(): void {
+
+          this.initMap();
+
+     }
+
+
+}
