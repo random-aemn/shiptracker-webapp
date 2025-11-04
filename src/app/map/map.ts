26b93dee6422b3bf4cb87fd0da54b71ffebeb5ce
random-aemn <82000272+random-aemn@users.noreply.github.com>
2025-11-03 19:29:08 -0500

    cleaned up commented-out code

--- src/app/map/map.ts
+++ src/app/map/map.ts
@@ -31,14 +31,6 @@ export class Map  implements AfterViewInit, OnInit  {
 
   ngOnInit(): void {
 
-    
-    // this.jsonReader.getFred().subscribe(
-    //   (data: any) => {
-    //     this.geoJsonData = data;
-    //     console.log("in ngOnInit, this is the geojson data");
-    //     console.info(this.geoJsonData);
-    //   }
-    // )
 
   }
 
@@ -69,8 +61,6 @@ export class Map  implements AfterViewInit, OnInit  {
 
   private initMap(): void {
 
-    // const cbCenter: L.LatLngExpression = [38.5214, -77.1050]
-
     this.map = L.map('cbMap', {
       center: this.cbCenter,
       zoom: 7
@@ -85,26 +75,13 @@ export class Map  implements AfterViewInit, OnInit  {
     tiles.addTo(this.map);
 
 
-    // This method has the stringify and console.log statements - service call returns a subscription
-    // let cbFeatureCollection = this.jsonReader.getChesapeakeBayOutline();
-
-
-    // let cbBoundingBox = this.jsonReader.getFred;
-
-
-
     // Service call returns an observable to which we subscribe
     this.jsonReader.getFred().subscribe(
     (rawData: FeatureCollection) => {
 
 
       console.log("accessing rawData....: ");
-      // This is working and the feature collection is working AS FAR AS IT IS DEFINED - believe the FeatureCollection we imported does not define FeatureCollection past geometry
       console.info(rawData.features[0].geometry);
-
-
-          // console.log("in the map.ts initMap method, I'm extracting the data from the geoJson");
-          // console.log(this.geoJsonData.features[0].geometry.coordinates[0]);
 
           this.geoJsonData = rawData;
           console.log("this is geoJsonData from the subscription/observable");
@@ -116,21 +93,6 @@ export class Map  implements AfterViewInit, OnInit  {
 
     )
 
-
-
-    // let cbAntBox = new L.Polyline(latLngs, {color: 'blue', weight: 2}).addTo(this.map); 
-    // L.geoJSON(this.geoJsonData).addTo(this.map);
-    // L.geoJSON(this.ServiceCallGeoJsonData).addTo(this.map);
-
-    // L.geoJSON(this.cbOutline, {
-    // style: function (feature) {
-    //     return {color: 'red', weight: 1,
-    //         fill: false
-    //     };
-    //   }
-    // }).addTo(this.map);
-
-
   }
 
 
@@ -139,22 +101,6 @@ export class Map  implements AfterViewInit, OnInit  {
 
       console.log("I'm in the ngAfterViewInit method calling the this.initMap function");
       this.initMap();
-
-      // L.geoJSON(this.geoJsonData).addTo(this.map);
-
-
-    // let cbAntBox = new L.Polyline(latLngs, {color: 'blue', weight: 2}).addTo(this.map); 
-    // L.geoJSON(this.geoJsonData).addTo(this.map);
-    // L.geoJSON(this.ServiceCallGeoJsonData).addTo(this.map);
-
-    // L.geoJSON(this.cbOutline, {
-    // style: function (feature) {
-    //     return {color: 'red', weight: 1,
-    //         fill: false
-    //     };
-    //   }
-    // }).addTo(this.map);
-
 
      }
 
